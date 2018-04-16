6e29f53c1524aebc0ed7b70805c48ef3a04e6ac2
Ibby Hadeed <ibby93@gmail.com>
2018-04-16 05:57:49 -0400

    Update

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {NavController, Platform} from 'ionic-angular';
+
+declare const cordova: any;
+
+export function stripe() {
+  return cordova.plugins.stripe;
+}
 
 @Component({
   selector: 'page-home',
@@ -7,8 +13,42 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(private platform: Platform) {
+    platform.ready()
+      .then(() => {
+        stripe().setPublishableKey('pk_test_o9rrGzeyADseN9nYqFc5LHae');
+      });
+  }
 
+  getToken() {
+    stripe().createCardToken({
+      number: '4242424242424242',
+      name: 'Ibby Hadeed',
+      cvc: '424',
+      expMonth: 5,
+      expYear: 2020
+    }, (token) => {
+      console.log('Token is', token);
+    });
+  }
+
+  testApplePay() {
+    stripe().payWithApplePay({
+      merchantId: 'merchant.ca.zyra',
+      country: 'CA',
+      currency: 'CAD',
+      items: [
+        {
+          label: 'Test item',
+          amount: 100
+        }
+      ]
+    }, (token: any, callback: Function) => {
+      console.log('Token is ', token);
+      // send token to backend & process payment
+
+      callback(true);
+    });
   }
 
 }
